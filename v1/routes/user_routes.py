4622e2a74cbcab67053e203dfcfff4f8f4c7139c
JustinzDev <fullyz1532@gmail.com>
2023-07-12 12:15:22 +0700

    update response status

--- v1/routes/user_routes.py
+++ v1/routes/user_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 # Controllers
 from v1.controllers.user_controllers import get_users, get_user, create_user
@@ -9,8 +9,13 @@ from v1.models.user_models import User
 router = APIRouter()
 
 @router.get('/users')
-async def read_users():
-    return await get_users()
+async def read_users(response: Response):
+    users = await get_users()
+    if not users:
+        response.status_code = 404
+        return { "type": "not_found" }
+    else:
+        return await get_users()
 
 @router.get('/user/{userID}')
 async def read_user(userID: int):
